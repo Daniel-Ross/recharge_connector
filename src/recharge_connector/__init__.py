58ca3a7d91ef7053ddbea7522c0f0264e696b9db
Dan Ross <daniel.ross@aop.com>
2025-05-15 15:38:56 -0500

    Bump version to 1.1.2 and include pull_orders_by_ids in __all__

--- src/recharge_connector/__init__.py
+++ src/recharge_connector/__init__.py
@@ -2,7 +2,6 @@ __version__ = "1.0.0"
 __author__ = "Daniel Ross"
 
 from .get_subs import pull_active_subs
+from .get_orders import pull_orders_by_ids
 
-__all__ = [
-   "pull_active_subs" 
-]
+__all__ = ["pull_active_subs", "pull_orders_by_ids"]
